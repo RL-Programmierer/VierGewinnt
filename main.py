c002a8317f0eefbfeb2bf33ac9890c46948af31b
BuildTools <unconfigured@null.spigotmc.org>
2023-12-19 22:03:52 +0100

    Just a simple Test

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from tkinter import *
+from pygame import *
 
 gui = Tk()
 gui.geometry('750x500')
 gui.title('Start')
 
+
 class Player:
-    x = 0
-    y = 0
+    verticalRow = 0
+    horizontalRow = 0
+    color = ""
+    name = ""
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, color, name):
+        self.verticalRow = 0
+        self.horizontalRow = 0
+        self.color = color
+        self.name = name
 
-    def move(self, x, y):
-        self.x = x
-        self.y = y
+    def move(self, verticalRow, horizontalRow):
+        self.verticalRow = verticalRow
+        self.horizontalRow = horizontalRow
 
-
-nameplayer1 = 0
-nameplayer2 = 0
 
 canvas_width = 200
 canvas_height = 40
@@ -28,17 +31,17 @@ myCanvas.pack(expand=YES, fill=BOTH)
 myCanvas.create_oval(100, 50, 160, 110, fill="yellow")
 myCanvas.create_oval(100, 150, 160, 210, fill="red")
 
+
+def start():
+    print('Sehr Gutt')
+
+
 but1 = Button(gui, width=20, height=6)
 but1["text"] = "Start"
-but1["command"] = 'start'
+but1["command"] = start()
 but1.pack()
 
 but1.place(x=275, y=300)
-
-
-def start():
-    print('ok')
-
 
 tf_player1 = Entry(gui)
 tf_player1.pack()
@@ -47,5 +50,8 @@ tf_player1.place(x=175, y=70)
 tf_player2 = Entry(gui)
 tf_player2.pack()
 tf_player2.place(x=175, y=170)
+
+nameplayer1 = Player("yellow", tf_player1.get())
+nameplayer2 = Player("red", tf_player2.get())
 
 gui.mainloop()
