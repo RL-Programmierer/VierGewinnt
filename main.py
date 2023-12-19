c9596a317efc2cb379732d707948c459acf5ff6f
BuildTools <unconfigured@null.spigotmc.org>
2023-12-19 21:46:00 +0100

    Player Class

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ gui = Tk()
 gui.geometry('750x500')
 gui.title('Start')
 
+class Player:
+    x = 0
+    y = 0
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+
 nameplayer1 = 0
 nameplayer2 = 0
 
