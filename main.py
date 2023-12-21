6f1522566bdb5c8aeb04f5877246b6485da5c7c5
RL Programmierer <154360239+RL-Programmierer@users.noreply.github.com>
2023-12-21 15:28:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
-from pygame import *
+
 
 gui = Tk()
 gui.geometry('750x500')
 gui.title('VierGewinnt')
+
 
 
 class Player:
@@ -23,35 +24,52 @@ class Player:
         self.horizontalRow = horizontalRow
 
 
+
+#Kreise im Startgame Bildschirm
 canvas_width = 200
 canvas_height = 40
 
 myCanvas = Canvas(width=canvas_width, height=canvas_height, bg='grey')
 myCanvas.pack(expand=YES, fill=BOTH)
-myCanvas.create_oval(100, 50, 160, 110, fill="yellow")
-myCanvas.create_oval(100, 150, 160, 210, fill="red")
+oval1 = myCanvas.create_oval(100, 50, 160, 110, fill="yellow")
+oval2 = myCanvas.create_oval(100, 150, 160, 210, fill="red")
 
 
-def startGame():
-    print('Test')
 
 
-but1 = Button(gui, width=20, height=6)
+
+
+#Button und Textfeld code(Startbildschirm)
+
+but1 = Button(gui, width=20, height=6, bg='grey')
 but1["text"] = "Start"
-but1["command"] = startGame()
-but1.pack()
-
+but1["command"] = lambda:startGame()
 but1.place(x=275, y=300)
 
-tf_player1 = Entry(gui)
-tf_player1.pack()
+
+
+tf_player1 = Entry(gui, bg='grey')
 tf_player1.place(x=175, y=70)
 
-tf_player2 = Entry(gui)
-tf_player2.pack()
+tf_player2 = Entry(gui, bg='grey')
+
 tf_player2.place(x=175, y=170)
 
 player1 = Player("yellow", tf_player1.get())
 player2 = Player("red", tf_player2.get())
+
+
+
+def startGame():
+
+    myCanvas.delete(oval1)
+    myCanvas.delete(oval2)
+    tf_player1.destroy()
+    tf_player2.destroy()
+    but1.destroy() 
+
+
+
+
 
 gui.mainloop()
