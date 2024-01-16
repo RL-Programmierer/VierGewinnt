6c9432d996224e0d9b6ac560435d479a22127279
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 19:22:16 +0100

    Kurzes Update

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ gui.title('VierGewinnt')
 
 
 class VierGewinntFeld:
-    # ToDo: Add a Canvas for every Feld
-
-    x = 0
-    y = 0
+    feld = 0
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def move(self, x, y):
-        self.x = x
-        self.y = y
-
-
+    def createCanvas(self):
+        if self.feld == 0:
+            self.feld = Canvas(width=gui.winfo_screenwidth() / self.x, height=gui.winfo_screenheight() / self.y,
+                               bg='yellow', highlightthickness=5, highlightbackground="black")
+            self.feld.pack(expand=YES, fill=BOTH)
+        return self.feld
 
 
 class Player:
@@ -31,10 +29,6 @@ class Player:
     def __init__(self, playerColor, name):
         self.playerColor = playerColor
         self.name = name
-
-    def move(self, x, y):
-        self.playerField.move(x, y)
-
 
 
 # Kreise im Startgame Bildschirm
@@ -77,7 +71,10 @@ def startGame():
     setupPlayerListBar()
 
 
-spielfeld = [[], [], [], [], [], [], []]
+verticalFeldNumber = 7
+horizontalFeldNumber = 6
+
+spielfeld = []
 
 
 def setupPlayerListBar():
@@ -87,131 +84,15 @@ def setupPlayerListBar():
 
 def setupSpielFeld():
     # ToDo: implement this
+
+    for x in range(verticalFeldNumber):
+        print(x)
+        for y in range(horizontalFeldNumber):
+            feld = VierGewinntFeld(x, y)
+            feld.createCanvas()
+            spielfeld.append(feld)
+            print(y)
     print("Setting Up Spielfeld!")
-
-
-gui.mainloop()
-
-
-
-
-
-#neues#########################################################################
-
-
-
-from tkinter import *
-
-
-gui = Tk()
-gui.geometry('750x500')
-gui.title('VierGewinnt')
-endbildschirm='aus'
-
-
-class Player:
-    verticalRow = 0
-    horizontalRow = 0
-    playerColor = ""
-    name = ""
-
-    def __init__(self, playerColor, name):
-        self.verticalRow = 0
-        self.horizontalRow = 0
-        self.playerColor = playerColor
-        self.name = name
-
-    def move(self, verticalRow, horizontalRow):
-        self.verticalRow = verticalRow
-        self.horizontalRow = horizontalRow
-
-
-
-#Kreise im Startgame Bildschirm
-canvas_width = 200
-canvas_height = 40
-
-myCanvas = Canvas(width=canvas_width, height=canvas_height, bg='grey')
-myCanvas.pack(expand=YES, fill=BOTH)
-oval1 = myCanvas.create_oval(100, 50, 160, 110, fill="yellow")
-oval2 = myCanvas.create_oval(100, 150, 160, 210, fill="red")
-
-
-
-
-
-
-#Button und Textfeld code(Startbildschirm)
-
-but1 = Button(gui, width=20, height=6, bg='grey')
-but1["text"] = "Start"
-but1["command"] = lambda:startGame()
-but1.place(x=275, y=300)
-
-
-
-tf_player1 = Entry(gui, bg='grey')
-tf_player1.place(x=175, y=70)
-
-tf_player2 = Entry(gui, bg='grey')
-
-tf_player2.place(x=175, y=170)
-
-player1 = Player("yellow", tf_player1.get())
-player2 = Player("red", tf_player2.get())
-
-
-
-def startGame():
-
-    myCanvas.delete(oval1)
-    myCanvas.delete(oval2)
-    tf_player1.destroy()
-    tf_player2.destroy()
-    but1.destroy()
-
-#Button für neue Runde
-while endbildschirm=='an':
-
-    but2 = Button(gui, width=20, height=6, bg='grey')
-    but2["text"] = "Start"
-    but2["command"] = lambda:newGame()
-    but2.place(x=275, y=300)
-    endbildschirm='aus'
-
-    def newGame():
-        
-
-        oval1 = myCanvas.create_oval(100, 50, 160, 110, fill="yellow")
-        oval2 = myCanvas.create_oval(100, 150, 160, 210, fill="red")
-        myCanvas.pack()
-
-        but1 = Button(gui, width=20, height=6, bg='grey')
-        but1["text"] = "Start"
-        but1["command"] = lambda:startGame()
-        but1.place(x=275, y=300)
-
-
-
-
-
-#hallo
-
-
-
-
-
-
-
-
-player1 = Player("yellow", tf_player1.get())
-player2 = Player("red", tf_player2.get())
-
-
-
-
-
-
 
 
 gui.mainloop()
