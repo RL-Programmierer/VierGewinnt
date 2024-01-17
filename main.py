4c4b9918799288cdf392958e051bf1421bd0a84c
Roboter007 <92993787+Roboter007@users.noreply.github.com>
2024-01-17 16:59:40 +0100

    Last Player Bar Commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ w, h = gui.winfo_screenwidth(), gui.winfo_screenheight()
 gui.geometry("%dx%d+0+0" % (w, h))
 gui.title('VierGewinnt')
 
+
 class VierGewinntFeld:
     feld = 0
 
@@ -14,24 +15,19 @@ class VierGewinntFeld:
 
     def createCanvas(self, background):
         if self.feld == 0:
-            this_width = 1920 / self.x
-            this_height = 1080 / self.y
-            self.feld = background.create_rectangle(this_height - 5, this_height - 5, this_width + 5,
-                                                    this_height + 5)
+            this_width = 1920 / self.y
+            this_height = 1080 / self.x
+            self.feld = background.create_rectangle(this_height - 20, this_height - 20, this_width + 20,
+                                                    this_height + 20, fill="white")
         return self.feld
 
 
 class Player:
     spielerFeld = VierGewinntFeld(0, 0)
-    playerColor = ""
-    name = ""
 
     def __init__(self, playerColor, name):
         self.playerColor = playerColor
         self.name = name
-
-    def setPlayerField(self, spielerFeld):
-        self.spielerFeld = spielerFeld
 
 
 # Kreise im Startgame Bildschirm
@@ -42,6 +38,7 @@ background_start = Canvas(width=canvas_width, height=canvas_height, bg='grey')
 background_start.pack(expand=YES, fill=BOTH)
 oval1 = background_start.create_oval(100, 50, 160, 110, fill="yellow")
 oval2 = background_start.create_oval(100, 150, 160, 210, fill="red")
+background_start.create_text(960, 200, text="VierGewinnt", fill="black", font=("Purisa", 100))
 
 # Button und Textfeld code(Startbildschirm)
 
@@ -54,24 +51,31 @@ tf_player1 = Entry(gui, bg='grey')
 tf_player1.place(x=175, y=70)
 
 tf_player2 = Entry(gui, bg='grey')
-
 tf_player2.place(x=175, y=170)
 
-player1 = Player("yellow", tf_player1.get())
-player2 = Player("red", tf_player2.get())
+
+def getPlayer1():
+    return Player("yellow", tf_player1.get())
+
+
+def getPlayer2():
+    return Player("yellow", tf_player2.get())
 
 
 def startGame():
-    background_start.delete(oval1)
-    background_start.delete(oval2)
-    background_start.destroy()
-    tf_player1.destroy()
-    tf_player2.destroy()
-    but1.destroy()
-    background = Canvas(width=gui.winfo_screenwidth(), height=gui.winfo_screenheight(), bg='grey')
-    background.pack(expand=YES, fill=BOTH)
-    setupSpielFeld(background)
-    setupPlayerListBar(background)
+    if getPlayer1().name != "" and getPlayer2().name != "":
+        background_start.delete(oval1)
+        background_start.delete(oval2)
+        background_start.destroy()
+        but1.destroy()
+        background = Canvas(width=gui.winfo_screenwidth(), height=gui.winfo_screenheight(), bg='grey')
+        background.pack(expand=YES, fill=BOTH)
+        setupSpielFeld(background)
+        setupPlayerListBar(background)
+        tf_player1.destroy()
+        tf_player2.destroy()
+    else:
+        print("Keine Namen sind gesetzt")
 
 
 verticalFeldNumber = 7
@@ -81,13 +85,10 @@ spielfeld = []
 
 
 def setupPlayerListBar(background):
-
     background.create_rectangle(0, 0, 1920, 60, fill="#585B5F")
-    background.create_text(820,30, text='player1.name' , fill='#000000',font=('Purisa', 18))
-    background.create_text(960,30, text='-' , fill='#000000',font=('Purisa', 22))
-    background.create_text(1100,30, text='player2.name' , fill='#000000',font=('Purisa', 18))
-
-
+    background.create_text(820, 30, text=getPlayer1().name, fill='#000000', font=('Purisa', 18))
+    background.create_text(960, 30, text='-', fill='#000000', font=('Purisa', 22))
+    background.create_text(1100, 30, text=getPlayer2().name, fill='#000000', font=('Purisa', 18))
 
 
 def setupSpielFeld(background):
