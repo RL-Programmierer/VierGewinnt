b449a4051b5abbd5f720c4949445669141789a26
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 21:42:31 +0100

    Runden Mechanik

--- main.py
+++ main.py
@@ -1,39 +1,87 @@
 from tkinter import *
+from tkinter import font as tkFont
 
 gui = Tk()
 w, h = gui.winfo_screenwidth(), gui.winfo_screenheight()
 gui.geometry("%dx%d+0+0" % (w, h))
 gui.title('VierGewinnt')
-activeplayer = 1
+spielerAnDerReihe = 1
+
+print('max. Länge:', w)
+print('max. Höhe:', h)
 
 # Feld Größe Einstellung
 size = 130
 
+# Spielfeldgröße
 horizontalFeldNumber = 7
 verticalFeldNumber = 8
+
+roundNumber = 0
+
+
+class PlayerListBar:
+    def __init__(self, background):
+        global spielerAnDerReihe
+        self.background = background
+        self.Rechteck = background.create_rectangle(0, 0, 1920, 60, fill="#585B5F")
+        self.Bindestrich = background.create_text(960, 30, text='-', fill='#000000', font=('Purisa', 22))
+
+        if spielerAnDerReihe == 1:
+            self.Spieler1 = background.create_text(820, 30, text=getPlayer1().name, fill='#000000', font=('Purisa', 18))
+        else:
+            self.Spieler1 = background.create_text(820, 30, text=getPlayer1().name, fill='#847B79', font=('Purisa', 18))
+
+        if spielerAnDerReihe == 2:
+            self.Spieler2 = background.create_text(1100, 30, text=getPlayer2().name, fill='#000000',
+                                                   font=('Purisa', 18))
+        else:
+            self.Spieler2 = background.create_text(1100, 30, text=getPlayer2().name, fill='#847B79',
+                                                   font=('Purisa', 18))
+
+    def tauscheSpielerAnDerReihe(self):
+        global spielerAnDerReihe
+
+        if spielerAnDerReihe == 1:
+            spielerAnDerReihe = spielerAnDerReihe + 1
+        elif spielerAnDerReihe == 2:
+            spielerAnDerReihe = spielerAnDerReihe - 1
+        else:
+            print('Error!!! SpielerAnDerReihe:', spielerAnDerReihe)
+
+        if spielerAnDerReihe == 1:
+            self.background.itemconfig(self.Spieler1, fill='#847B79')
+            self.background.itemconfig(self.Spieler2, fill='#000000')
+        elif spielerAnDerReihe == 2:
+            self.background.itemconfig(self.Spieler1, fill='#000000')
+            self.background.itemconfig(self.Spieler2, fill='#847B79')
 
 
 # Repräsentiert ein Feld
 class VierGewinntFeld:
-    feld = 0
+    farbe = 'white'
 
-    def __init__(self, feldX, feldY):
+    def __init__(self, background, feldX, feldY):
         self.feldX = feldX
         self.feldY = feldY
+        self.background = background
 
-    def createCanvas(self, background):
-        if self.feld == 0:
-            feld_y = 50 + size * self.feldX
-            feld_x = 375 + size * self.feldY
+        feld_y = 50 + size * self.feldX
+        feld_x = 375 + size * self.feldY
 
-            self.feld = background.create_rectangle(feld_x, feld_y, feld_x + size,
-                                                    feld_y + size, fill="white")
-        return self.feld
+        self.feld = background.create_rectangle(feld_x, feld_y, feld_x + size,
+                                                feld_y + size, fill=self.farbe)
+
+    def setColor(self, color):
+        self.farbe = color
+        self.background.itemconfig(self.feld, fill=color)
+
+    def getColor(self):
+        return self.farbe
 
 
 # Repräsentiert einen Spieler
 class Player:
-    spielerFeld = VierGewinntFeld(0, 0)
 
     def __init__(self, playerColor, name):
         self.playerColor = playerColor
@@ -46,29 +94,30 @@ canvas_height = 40
 
 
 # create
-def createPlayerChip(canvas, x, y, dx, dy, color):
-    chip = canvas.create_oval(x, y, dx, dy, fill=color)
+def createPlayerChip(canvas, x, y, chipSize, color):
+    chip = canvas.create_oval(x, y, x + chipSize, y + chipSize, fill=color)
     return chip
 
 
 background_start = Canvas(width=canvas_width, height=canvas_height, bg='grey')
 background_start.pack(expand=YES, fill=BOTH)
-oval1 = createPlayerChip(background_start, 100, 50, 160, 110, "yellow")
-oval2 = createPlayerChip(background_start, 100, 150, 160, 210, "red")
+oval1 = createPlayerChip(background_start, 850, 310, 60, "yellow")
+oval2 = createPlayerChip(background_start, 850, 410, 60, "red")
 background_start.create_text(960, 200, text="VierGewinnt", fill="black", font=("Purisa", 100))
 
 # Button und Textfeld code(Startbildschirm)
 
-but1 = Button(gui, width=20, height=6, bg='grey')
+helv36 = tkFont.Font(family='Helvetica', size=36, weight='bold')
+but1 = Button(gui, width=30, height=6, bg='grey')
 but1["text"] = "Start"
 but1["command"] = lambda: startGame()
-but1.place(x=920, y=540)
+but1.place(x=890, y=540)
 
-tf_player1 = Entry(gui, bg='grey')
-tf_player1.place(x=175, y=70)
+tf_player1 = Entry(gui, bg='grey', width=30)
+tf_player1.place(x=950, y=330)
 
-tf_player2 = Entry(gui, bg='grey')
-tf_player2.place(x=175, y=170)
+tf_player2 = Entry(gui, bg='grey', width=30)
+tf_player2.place(x=950, y=430)
 
 
 def getPlayer1():
@@ -93,6 +142,11 @@ def startGame():
         tf_player2.destroy()
 
         buttemporaer = Button(gui, width=20, height=6, bg='grey')
+        buttemporaer["text"] = "nextRoundTest"
+        buttemporaer["command"] = lambda: nextRound()
+        buttemporaer.place(x=500, y=740)
+
+        buttemporaer = Button(gui, width=20, height=6, bg='grey')
         buttemporaer["text"] = "temporaer"
         buttemporaer["command"] = lambda: restartGame(background)
         buttemporaer.place(x=1020, y=740)
@@ -102,27 +156,20 @@ def startGame():
 
 
 spielfeld = []
+playerListBar = 0
 
 
 def setupPlayerListBar(background):
-    global var1
-    global var2
-    global var3
-    global var4
+    global playerListBar
+    playerListBar = PlayerListBar(background)
 
-    var1 = background.create_rectangle(0, 0, 1920, 60, fill="#585B5F")
 
-    if activeplayer == 1:
-        var2 = background.create_text(820, 30, text=getPlayer1().name, fill='#000000', font=('Purisa', 18))
-    else:
-        var3 = background.create_text(820, 30, text=getPlayer1().name, fill='#847B79', font=('Purisa', 18))
-
-    var4 = background.create_text(960, 30, text='-', fill='#000000', font=('Purisa', 22))
-
-    if activeplayer == 2:
-        background.create_text(1100, 30, text=getPlayer2().name, fill='#000000', font=('Purisa', 18))
-    else:
-        background.create_text(1100, 30, text=getPlayer2().name, fill='#847B79', font=('Purisa', 18))
+def nextRound():
+    global roundNumber
+    global playerListBar
+    roundNumber = roundNumber + 1
+    print('Round Number:', roundNumber)
+    playerListBar.tauscheSpielerAnDerReihe()
 
 
 def setupSpielFeld(background):
@@ -130,9 +177,10 @@ def setupSpielFeld(background):
     for x in range(1, horizontalFeldNumber):
         # y Koordinate Berechnung
         for y in range(1, verticalFeldNumber):
-            feld = VierGewinntFeld(x, y)
-            feld.createCanvas(background)
+            feld = VierGewinntFeld(background, x, y)
             spielfeld.append(feld)
+            if x == 1:
+                feld.setColor('yellow')
 
 
 # bei game reset
@@ -159,7 +207,7 @@ def restartGame(background):
 
     butrestart = Button(gui, width=50, height=10, bg='grey')
     butrestart["text"] = "Start"
-    butrestart["command"] = lambda: restartGame()
+    butrestart["command"] = lambda: restartGame(background)
     butrestart.place(x=820, y=500)
 
     background.delete(var1)
