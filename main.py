4afd01985ee4e8fa5c8ed516131a828b73a306f0
RL Programmierer <154360239+RL-Programmierer@users.noreply.github.com>
2024-02-29 21:23:29 +0100

    Mehr Kommentare

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ size = 130
 horizontalFeldNumber = 7
 verticalFeldNumber = 8
 
-roundNumber = 0
-
+# default Spieler Name
 playerName1 = 'Spieler 1'
 playerName2 = 'Spieler 2'
 
 
 # Repräsentiert die Spieler Leiste
 class PlayerListBar:
+
+    # erstellt die obere Leiste mit Spielernamen
     def __init__(self, background):
         global spielerAnDerReihe
         self.background = background
@@ -44,6 +45,7 @@ class PlayerListBar:
             self.Spieler2 = background.create_text(1100, 30, text=player2.name, fill='#847B79',
                                                    font=('Purisa', 18))
 
+    # tauscht den Spieler, der an der Reihe ist
     def tauscheSpielerAnDerReihe(self):
         global spielerAnDerReihe
 
@@ -62,16 +64,17 @@ class PlayerListBar:
             self.background.itemconfig(self.Spieler2, fill='#000000', font=('Purisa', 18, 'bold'))
 
 
-# Repräsentiert ein Feld
+# Repräsentiert ein Feld -> ein Viereck des Spielfelds
 class VierGewinntFeld:
-    farbe = 'blue'
 
+    # Erstellen des Vierecks
     def __init__(self, background, feldX, feldY):
         self.playerChip = None
         self.playerNumber = 0
         self.feldX = feldX
         self.feldY = feldY
         self.background = background
+        self.farbe = 'blue'
 
         feld_y = 100 + size * self.feldX
         feld_x = 375 + size * self.feldY
@@ -94,11 +97,13 @@ class VierGewinntFeld:
     def getPlayerNumber(self):
         return self.playerNumber
 
+    # Überprüfung, ob das Viereck von einem Spieler besetzt ist
     def isEmpty(self):
         return self.playerNumber == 0
 
+    # setzt den Spieler Chip und ersetzt den Spieler 0 → Spieler 0 = leeres Feld
     def placeChip(self, color, playerNumber):
-        if self.playerChip is not None:
+        if self.playerNumber != 0 and self.playerChip is not None:
             background.delete(self.playerChip)
         self.playerNumber = playerNumber
         feld_y = 100 + size * self.feldX
@@ -106,6 +111,7 @@ class VierGewinntFeld:
         offset = 20
         self.playerChip = createPlayerChip(self.background, feld_x + offset, feld_y + offset, size - offset * 2, color)
 
+    # löscht Spieler Chip, wenn es nicht ein leeres Feld
     def deleteChip(self):
         if self.playerChip is not None and not self.isEmpty():
             self.background.delete(self.playerChip)
@@ -114,6 +120,7 @@ class VierGewinntFeld:
 # Repräsentiert einen Spieler
 class Player:
 
+    # Initialisierung des Spielers
     def __init__(self, playerColor, name, playerNumber):
         self.playerNumber = playerNumber
         self.playerColor = playerColor
@@ -139,61 +146,79 @@ class Player:
 canvas_width = 200
 canvas_height = 40
 
+# default Spieler Farben
 colorOfPlayer1 = 'red'
 colorOfPlayer2 = 'yellow'
 
+# Spieler Variablen
 player1 = Player(colorOfPlayer1, playerName1, 1)
 player2 = Player(colorOfPlayer2, playerName2, 2)
 
+# Spieler an der Reihe
 spielerAnDerReihe = player1
+
+# Runden Nummer
+roundNumber = 0
+
+# Stellt dar, ob das Game restarted wurde oder nicht →
+# wird gebraucht um in startGame() die Buttons und usw vom Hauptmenu zu löschen
 restarted = False
 
 
-# create
+# Hilfsfunktion um einen Spieler Chip auf dem Bildschirm darzustellen
 def createPlayerChip(canvas, x, y, chipSize, color):
     chip = canvas.create_oval(x, y, x + chipSize, y + chipSize, fill=color)
     return chip
 
 
+# Erstellung des haupt Canvas
 background = Canvas(width=gui.winfo_screenwidth(), height=gui.winfo_screenheight(), bg='grey')
 background.pack(expand=YES, fill=BOTH)
 
 
+# öffnet das Coolor Chooser Menu für Spieler 1 und setzt die Farbe, wenn sie verändert wurde
 def chooseColorPlayer1():
     global colorOfPlayer1
     colorOfPlayer1 = colorchooser.askcolor(title="Choose color", initialcolor=colorOfPlayer1)[1]
     background.itemconfig(chip1, fill=colorOfPlayer1)
 
 
+# öffnet das Coolor Chooser Menu für Spieler 2 und setzt die Farbe, wenn sie verändert wurde
 def chooseColorPlayer2():
     global colorOfPlayer2
     colorOfPlayer2 = colorchooser.askcolor(title="Choose color", initialcolor=colorOfPlayer2)[1]
     background.itemconfig(chip2, fill=colorOfPlayer2)
 
 
+# setzen der Chip Variablen für die Spieler im Hauptmenü
 chip1 = createPlayerChip(background, 800, 310, 80, colorOfPlayer1)
 chip2 = createPlayerChip(background, 800, 410, 80, colorOfPlayer2)
 
+# Wenn der <Button-1> gedrückt wird (links Klick) →
+# wird die Farbe abgefragt über chooseColorPlayer1() / chooseColorPlayer2()
 background.tag_bind(chip1, '<Button-1>', lambda a: chooseColorPlayer1())
 background.tag_bind(chip2, '<Button-1>', lambda a: chooseColorPlayer2())
 
 vierGewinnt = background.create_text(960, 200, text="Vier Gewinnt", fill="black", font=("Purisa", 100))
-# Button und Textfeld code(Startbildschirm)
 
+# Start Button wird gesetzt
 startButton = Button(gui, width=30, height=6, bg='grey')
 startButton["text"] = "Start"
 startButton["command"] = lambda: startGame()
 startButton.place(x=890, y=540)
 
+# Textfeld für Spieler 1
 tf_player1 = Entry(gui, bg='grey', width=25, font=("Purisa", 14))
 tf_player1.place(x=900, y=340)
 tf_player1.insert(0, 'Spieler 1')
 
+# Textfeld für Spieler 2
 tf_player2 = Entry(gui, bg='grey', width=25, font=("Purisa", 14))
 tf_player2.place(x=900, y=440)
 tf_player2.insert(0, 'Spieler 2')
 
 
+# Funktion fürs Starten vom Vier Gewinnt Spiel
 def startGame():
     global player1
     global player2
@@ -227,10 +252,14 @@ def startGame():
         print("Keine Namen sind gesetzt")
 
 
+# Liste enthält weitere Listen → diese Listen repräsentieren eine vertikale Linie von den Vierecken
 spielfeld = []
+
+# Spielerleisten Variable
 playerListBar = 0
 
 
+# Überpruft, ob das Spiel restarted wurde → im Fall, das es restartet werden, muss werden die Hauptmenüelemente gelöscht
 def checkRestart():
     global restarted
     if restarted:
@@ -242,12 +271,15 @@ def checkRestart():
         tf_player2.destroy()
         restarted = False
 
+
+# erstellt die Spieler Leiste und setzt die Variable
 def setupPlayerListBar():
     global background
     global playerListBar
     playerListBar = PlayerListBar(background)
 
 
+# startet eine neue Runde
 def nextRound():
     global roundNumber
     global playerListBar
@@ -256,6 +288,7 @@ def nextRound():
     playerListBar.tauscheSpielerAnDerReihe()
 
 
+# alle Vierecke werden in einer Reihe zu einem Spielfeld zusammen gesetzt
 def setupSpielFeld():
     global background
     # x Koordinate Berechnung
@@ -268,14 +301,17 @@ def setupSpielFeld():
         spielfeld.append(horizontalLineList)
 
 
+# erstellt die Buttons, die in der Reihe einen neuen Chip hineinsetzten können
 def createControlButtons():
     for y in range(1, verticalFeldNumber):
         createControlButton(y)
 
 
+# Control Button Liste, um die Buttons spätr zu löschen
 buttons = []
 
 
+# Helferfunktion, um einen Control Button zu erstellen
 def createControlButton(row):
     button_x = 375 + size * row
     if row != 1:
@@ -287,6 +323,7 @@ def createControlButton(row):
     buttons.append(controlButton)
 
 
+# setzt den Chip an der richtigen stelle, damit die Physics funtkionieren
 def handlePlayerChip(row):
     feld = getPositonForChip(row)
     if feld is not None:
@@ -294,6 +331,7 @@ def handlePlayerChip(row):
         nextRound()
 
 
+# berechnet die richtige Stelle im Spielfeld für den Spielerchip
 def getPositonForChip(row):
     felderReihe = getFelderReihe(row - 1)
     for feld in reversed(felderReihe):
@@ -301,19 +339,23 @@ def getPositonForChip(row):
             return feld
 
 
-def getFelderReihe(index):
-    for i, j in enumerate(spielfeld):
-        if i == index:
-            return j
+# Helferfunktion, mit der man eine vertikale Reihe bekommt
+def getFelderReihe(vertikaleReihenNummer):
+    for index, vertikaleListe in enumerate(spielfeld):
+        if index == vertikaleReihenNummer:
+            return vertikaleListe
 
 
-def getFeld(horizontal, vertical):
-    for i, j in enumerate(spielfeld):
-        if i == horizontal:
-            for i2, j2 in enumerate(j):
-                if i2 == vertical:
-                    return j2
+# Bekommt das Feld, mit den angegebenen Koordinaten
+def getFeld(horizontaleNummer, vertikaleNummer):
+    for indexVertikal, vertikaleListe in enumerate(spielfeld):
+        if indexVertikal == horizontaleNummer:
+            for indexHorizontal, viereck in enumerate(vertikaleListe):
+                if indexHorizontal == vertikaleNummer:
+                    return viereck
 
+
+# löscht alle Elemente, die im Spielmenü da sind
 def deleteInGameItems():
     background.delete(playerListBar.Spieler1)
     background.delete(playerListBar.Spieler2)
@@ -321,15 +363,16 @@ def deleteInGameItems():
     background.delete(playerListBar.Rechteck)
     background.delete(vierGewinnt)
 
-    for i, j in enumerate(spielfeld):
-        for i2, j2 in enumerate(j):
-            background.delete(j2.feld)
-            background.delete(j2.playerChip)
+    for indexVertikal, listeVertikal in enumerate(spielfeld):
+        for indexHorizontal, viereck in enumerate(listeVertikal):
+            background.delete(viereck.feld)
+            background.delete(viereck.playerChip)
 
     for i, button in enumerate(buttons):
         button.destroy()
 
 
+# geht zum Startmenü zurück
 def backToStartMenu():
     global tf_player1
     global tf_player2
@@ -373,6 +416,7 @@ def backToStartMenu():
     restarted = True
 
 
+# erstellt, das Menü
 def createMenuButtons():
     buttemporaer3 = Button(gui, width=20, height=6, bg='grey')
     buttemporaer3["text"] = "Back To Start Menu"
